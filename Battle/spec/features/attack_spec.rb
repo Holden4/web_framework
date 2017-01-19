17c32fdaf7b9d8999d9ab61a9dc8aceaa11a59b3
Rob Holden <robholdenflag@gmail.com>
2017-01-19 14:36:55 +0000

    Extracted hit_points into player, removed spec feature for hit points and created tests for player

--- Battle/spec/features/attack_spec.rb
+++ Battle/spec/features/attack_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
 feature "Players attacking" do
-  binding.pry
+
   scenario "Expect players to attack and confirm" do
     sign_in_and_play
-    click_button 'Attack player 2'
-    expect(page).to have_content 'Attacked player 2!'
+    click_button 'Attack Player 2'
+    expect(page).to have_content 'Rob is attacking!'
   end
 end
